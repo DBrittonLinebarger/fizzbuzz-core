ba16a20057e5590090c1cf193b07f08f8258b22c
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-05-22 14:25:49 -0600

    Added Javadoc comments and generated HTML.

--- src/edu/cnm/deepdive/Main.java
+++ src/edu/cnm/deepdive/Main.java
@@ -1,7 +1,20 @@
 package edu.cnm.deepdive;
 
+/**
+ * Exercises (as a simple console application) the methods of the {@link FizzBuzz} class.
+ *
+ * @author Denelle Britton Linebarger &amp; Deep Dive Coding Java+Android Cohort 7.
+ * @version 1.0.0
+ */
 public class Main {
 
+  private Main() {}
+
+  /**
+   * Computes and displays the fizz buzz values that correspond to the input values of 1&ndash;100.
+   *
+   * @param args Command line arguments (ignored).
+   */
   public static void main(String[] args) {
     for (int i = 1;  i <= 100; i++) {
       System.out.println(FizzBuzz.fizzBuzzValue(i));
